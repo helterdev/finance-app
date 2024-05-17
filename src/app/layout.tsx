e2e59cac99170aa01cb9877886bc3f118bf1df61
helter <heltherv@gmail.com>
2024-05-16 23:00:25 -0500

    #5.5 sin index en el body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${inter.className} z-50`}>
+      <body className={`${inter.className}`}>
         {/* <Background /> */}
         <Navbar />
         {children}
